ac0db23c89b091eb660d96605d73bdc606cb35a3
APRIMR <aprimregmi24@gmail.com>
2024-09-24 20:54:38 +0545

    fix icons files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className=' h-14 w-full bg-slate-800 flex justify-between items-center px-10 md:px-32 md:h-12'>
       <div className='h-full flex items-center gap-1 '>
-        <img src="../../public/aregGPT.png" alt="logo" className='h-6 w-6 rounded rotate-12'  />
+        <img src="../assets/aregGPT.png" alt="logo" className='h-6 w-6 rounded rotate-12'  />
         <p id='logo'>reGen.ai</p>
       </div>
       <div className='flex gap-5 text-xl text-gray-300 '>
