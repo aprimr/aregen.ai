b77864fcf17b60d2778ca48b7f4d083f2d8a3f88
APRIMR <aprimregmi24@gmail.com>
2024-09-26 20:03:41 +0545

    change font size

--- src/App.jsx
+++ src/App.jsx
@@ -42,14 +42,14 @@ function App() {
   return (
     <Home>
       <Navbar />
-        {data.length==0&&<Body />} 
+      {data.length==0&&<Body />} 
       <div className='w-full px-2 mt-2 mb-20 pb-20'>
         {data.map((entry, index) => (
           <div key={index} className='mb-3'>
             {/* Question */}
             <div className='h-auto w-full mb-3 flex justify-end'>
               <div className='h-auto w-auto flex gap-2'>
-                <div className='h-full w-full px-6 py-2 text-sm bg-green-300 rounded-3xl rounded-tr-lg'>
+                <div className='h-full w-full px-6 py-2 text-sm bg-green-300  text-gray-800 rounded-3xl rounded-br-md md:text-lg'>
                   {entry.que}
                 </div>
                 <img src={profile} alt="logo" className='h-9 w-9 rounded-md' />
@@ -59,7 +59,7 @@ function App() {
             <div className='h-auto w-full flex justify-start'>
               <div className='h-auto w-auto flex gap-2'>
                 <img src={aregGPT} alt="logo" className='h-9 w-9 rounded-md' />
-                <div className='h-auto w-auto px-6 py-2 text-sm bg-slate-600 text-slate-200 rounded-3xl rounded-tl-lg'>
+                <div className='h-auto w-auto px-6 py-2 text-sm bg-slate-600 text-slate-200 rounded-3xl rounded-tl-md md:text-lg'>
                   {entry.ans}
                 </div>
               </div>
